0ce9a20074b6270d46ec347aa38bf9585068358e
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-06 13:48:48 +0530

    Arrow Functions examples!

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
  * JavaScript
  */
 
-const names = ['Satvinder Jeet Singh', 'Lovely', 'Harman Singh', 'Ashmeet Singh'];
-const fullNames = names.map((name)  => `${name} Manchanda`);
-console.log(fullNames);
+const race = '100m Dash';
+const winners = ['Hunter Gath', 'Singa Song', 'Imda Bos'];
 
-const sayMyName = (name) => {
-  console.log(`Hello ${name}`);
-}
+const win = winners.map((winner, index) => ({
+  name: winner,
+  race,
+  place: index + 1,
+}));
 
-sayMyName('Harry');
+console.table(win);
+
+const ages = [23, 62, 45, 234, 2, 62, 234, 62, 34];
+const old = ages.filter(age => age >= 60);
+console.log(old);
